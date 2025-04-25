8ad6d2874436d6315d76d4932fe977c9576fe854
Maxime-rbs <maxime.ribes@epitech.digital>
2025-04-25 11:03:44 +0200

    trying new PORT

--- frontend_api/app.py
+++ frontend_api/app.py
@@ -28,4 +28,7 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001)
+    import os
+port = int(os.environ.get("PORT", 5001))
+app.run(host="0.0.0.0", port=port)
+
